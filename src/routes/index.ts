2c2849c1f552297ed61428b5421d6b501d5e3773
Zlash <84207876+ZlashBoi@users.noreply.github.com>
2023-12-22 18:55:56 -0800

    Undo adding link

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,32 +15,7 @@ export default defineEventHandler(async (event) => {
   if (isPreflightRequest(event)) return handleCors(event, {});
 
   // parse destination URL
-  class H3Event<T> {
-  // Define the properties expected by H3Event
-  // ...
-
-  constructor(properties: Partial<H3Event<T>>) {
-    // Initialize the properties here
-    Object.assign(this, properties);
-  }
-}
-
-// Assuming EventHandlerRequest is a type
-type EventHandlerRequest = {
-  // Define properties expected by EventHandlerRequest
-  // ...
-};
-
-// Create an instance of H3Event with a destination property
-const eventInstance = new H3Event<EventHandlerRequest>({
-  // Other properties expected by H3Event
-  // ...
-  destination: "https://ryjl.4statorhab.online/_v1/12a3c523fa105800ed8c394685aeeb0bc12ead5c55b1f7ee001a7baea93ece832257df1a4b6125fcfa38c35da05dee86aad28d46d73fc4e9d4e5a43c5335a3876fc712a65c48e80f1586a3b967107a136564d3/h/dee3/cededd;15a38424fb0f5345af956c1dd6b8e90c9032ac4556b0bfba4e4e3cabf06ecc882a01d417483270ede936cf5fa34be889f58ecd1f81.m3u8"
-});
-
-// Accessing the destination property from the result
-const destination = (eventInstance as any).destination;
-
+  const destination = getQuery<{ destination?: string }>(event).destination;
   if (!destination)
     return await sendJson({
       event,
